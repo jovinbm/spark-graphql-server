6919ef74fb9ed1703e22cb738ff8ad42675058ed
Jovin Mwilanga <jovinbeda@gmail.com>
2023-11-28 15:55:37 -0500

    return values as flat data

--- src/api/data/index.ts
+++ src/api/data/index.ts
@@ -7,27 +7,19 @@ import publishers from '../../tables/publisher.json';
 const router = express.Router();
 
 router.get('/data/books', (_req, res) => {
-  res.status(200).json({
-    data: books,
-  });
+  res.status(200).json(books);
 });
 
 router.get('/data/authors', (_req, res) => {
-  res.status(200).json({
-    data: authors,
-  });
-});
-
-router.get('/data/genres', (_req, res) => {
-  res.status(200).json({
-    data: genres,
-  });
+  res.status(200).json(authors);
 });
 
 router.get('/data/publishers', (_req, res) => {
-  res.status(200).json({
-    data: publishers,
-  });
+  res.status(200).json(publishers);
+});
+
+router.get('/data/genres', (_req, res) => {
+  res.status(200).json(genres);
 });
 
 export default router;
