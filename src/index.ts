d4b256f7fda3d0fd5a74027fb5b0858491c440b4
Jovin Mwilanga <jovinbeda@gmail.com>
2023-11-28 15:33:58 -0500

    remove unnecessary app.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,27 @@
-import { app } from './app';
+import cors from 'cors';
+import express from 'express';
+import morgan from 'morgan';
+import api from './api';
+import * as middlewares from './middlewares';
 import { logger } from './utils/logger';
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({
+    message: `🦄🌈✨👋🌎🌍🌏✨🌈🦄`,
+  });
+});
+
+app.use('/api', api);
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
